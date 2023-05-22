6ae72cad121b8397cf93b4a97a040d4e0e83f5e5
Yuri Fisyak <fisyak@bnl.gov>
2023-05-21 20:16:23 -0400

    Add TSpline5

--- macros/MakedNdx.C
+++ macros/MakedNdx.C
@@ -11,6 +11,41 @@
 #include "TCanvas.h"
 #include "TMultiGraph.h"
 TMultiGraph *mg = 0;
+TH1D *histL = 0;
+TSpline5 *spline5 = 0;
+//________________________________________________________________________________
+Double_t func(Double_t *x, Double_t *p = 0) {
+  return histL->Interpolate(x[0]);
+}
+//________________________________________________________________________________
+TF1 *F() {
+  return new TF1("interpolation",func,-2,5.1,0);
+}
+//________________________________________________________________________________
+Double_t funcS(Double_t *x, Double_t *p = 0) {
+  return spline5->Eval(x[0]);
+}
+//________________________________________________________________________________
+TF1 *S() {
+  return new TF1("Spline5Int",funcS,-2,5.1,0);
+}
+//________________________________________________________________________________
+struct MyDerivFunc { 
+   MyDerivFunc(TF1 * f): fFunc(f) {}
+   double operator() (double *x, double * )  const { 
+      return fFunc->Derivative(*x);
+   }
+   TF1 * fFunc; 
+};
+//________________________________________________________________________________
+TF1 *Derivative(TF1 *f1) {
+  MyDerivFunc * deriv = new MyDerivFunc(f1);
+  TString name("der_");
+  name += f1->GetName();
+  TF1 * f2 = new TF1(name,deriv, f1->GetXmin(), f1->GetXmax(), 0, "MyDerivFunc"); 
+  return f2;
+}
+//________________________________________________________________________________
 void MakedNdx() {
   const Char_t *FileNames[2] = {"/afs/rhic.bnl.gov/star/users/fisyak/WWW/star/PiD/Bichsel/P10sglYu11bsmu.mom",
 				"/afs/rhic.bnl.gov/star/users/fisyak/WWW/star/PiD/Bichsel/P10sglYu11bgmu.mom"};
@@ -29,6 +64,7 @@ void MakedNdx() {
     Double_t  x[N];
     Double_t  xL10[N];
     Double_t  y[N];
+    Double_t  yL[N];
     Double_t  dy[N];
     Double_t  dyHG[N];
     Double_t  dyHS[N];
@@ -52,6 +88,7 @@ void MakedNdx() {
       x[npoints] = X;
       xL10[npoints] = TMath::Log10(X);
       y[npoints] = Y;
+      yL[npoints] = TMath::Log(Y);
       Double_t dX = 0;
       if (npoints > 0) {dX = xL10[npoints] - xL10[npoints-1]; dXav += dX; dX2av += dX*dX;}
       //      printf("%i\t%i\t%f\t%f\t%f\t%f\n",nr,i,x[npoints],T,y[npoints],dX);
@@ -66,7 +103,7 @@ void MakedNdx() {
     TString Name(Form("dNdx%s",Tags[m]));
     Double_t xmin = xL10[0];
     Double_t xmax = xL10[npoints-1];
-#if 0
+#if 1
     Double_t *xBins = new Double_t [npoints+1];
     for (i = 0; i <= npoints; i++) {
       if (i == 0)            xBins[i] = x[i]   - 0.5*(x[i+1] - x[i]); 
@@ -83,8 +120,8 @@ void MakedNdx() {
     hist->SetMarkerStyle(21);
     hist->GetXaxis()->SetTitle("#beta#gamma");
     hist->GetYaxis()->SetTitle("dN/dx (collision/cm)");
-    TCanvas *c1 = new TCanvas("c1","c1");
-    c1->SetLogx(1);
+    TCanvas *c4 = new TCanvas("c4","c4");
+    c4->SetLogx(1);
     hist->Draw("");
 #if 0
     TGraph *gr = new TGraph(npoints, x, y);
@@ -106,9 +143,13 @@ void MakedNdx() {
     TCanvas *c2 = new TCanvas("c2","c2");
     Name = Form("dNdxL10%s",Tags[m]);
     TH1D *histD = new TH1D(Name,"dNdx versus Log _{10} (#beta#gamma)",npoints,xmin-dXav/2,xmax+dXav/2);
+    Name = Form("LndNdxL10%s",Tags[m]);
+    histL = new TH1D(Name,"Log(dNdx) versus Log _{10} (#beta#gamma)",npoints,xmin-dXav/2,xmax+dXav/2);
     cout << "Create hitogram " << histD->GetName() << endl;
     for (i = 0; i < npoints; i++) {
       histD->SetBinContent(i+1,y[i]);
+      histL->SetBinContent(i+1,yL[i]);
+      histL->SetBinError(i+1,0.001);
       cout << i+1 << " x = " << xL10[i] << "\ty = " << y[i] << "\tbin center " << histD->GetBinCenter(i+1) << "\tcont = " << histD->GetBinContent(i+1) << endl;
     }
     histD->SetLineColor(3+2*m);
@@ -118,14 +159,29 @@ void MakedNdx() {
     histD->GetXaxis()->SetTitle("Log_{10} ( #beta#gamma )");
     histD->GetYaxis()->SetTitle("dN/dx (collision/cm)");
     histD->Draw();
-    TH1D *histS = new TH1D(*histD);
-    histS->SetName(Name + "Smoth");
-    histS->SetLineColor(4+2*m);
-    histS->SetLineWidth(4);
-    histS->SetMarkerColor(4);
-    histS->SetMarkerStyle(21);
-    histD->Smooth();
-    histD->Draw("same");
+    TCanvas *c1 = new TCanvas("c1","c1");
+    histL->SetLineColor(3+2*m);
+    histL->SetLineWidth(4);
+    histL->SetMarkerColor(3);
+    histL->SetMarkerStyle(21);
+    histL->GetXaxis()->SetTitle("Log_{10} ( #beta#gamma )");
+    histL->GetYaxis()->SetTitle("Log(dN/dx (collision/cm))");
+    histL->Draw();
+    TF1 *F1 = F();
+    Double_t a = xL10[0];
+    Double_t b = xL10[npoints-1];
+    Double_t eps = 2*(b-a)/npoints;
+    const Int_t npp = 15;
+    Double_t xx[npp] = {a,-1.5, -1., -0.5,  0., 0.5, 1., 1.5,2., 2.5, 3., 3.5, 4., 4.5,b};
+    //    spline5 = new TSpline5("spline5",xx,F1,npp,"b1a1b2e2", F1->Derivative(a), 0.,(F1->Derivative(a+eps)-F1->Derivative(a))/eps , 0.);
+    spline5 = new TSpline5("spline5",xx,F1,npp);
+    spline5->SetName(Name+"Spline5");
+    spline5->SetLineColor(2+2*m);
+    spline5->SetLineWidth(4);
+    spline5->SetMarkerColor(2);
+    spline5->SetMarkerStyle(21);
+    spline5->Draw("same");
+    spline5->Write();
 #if 0
     TGraph *grD = new TGraph(npoints, xL10, y);
     Name += "Gr";
