778d191af0164b47cf8f52258ddf532b8e153fd6
Yuri Fisyak <fisyak@bnl.gov>
2023-05-25 14:38:31 -0400

    Add defauls for y2023

---
+++ StarDb/RunLog/onl/starClockOnl.C
@@ -0,0 +1,11 @@
+TDataSet *CreateTable() { 
+  if (!TClass::GetClass("St_starClockOnl")) return 0;
+  starClockOnl_st row;
+  St_starClockOnl *tableSet = new St_starClockOnl("starClockOnl",1);
+  memset(&row,0,tableSet->GetRowSize());
+  row.runNumber	 =    2214033; // run number  ;
+  row.time	 =  996787667; // unix time of entry  ;
+  row.frequency	 =    9399962; // local clock
+  tableSet->AddAt(&row);
+ return (TDataSet *)tableSet;
+}
