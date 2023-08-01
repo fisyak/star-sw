2e99a5ce6ac0f15fa698f806b5c24b5cabde9d8c
Yuri Fisyak <fisyak@bnl.gov>
2023-08-01 09:50:22 -0400

    ostream for gcc13

--- StRoot/TPCCATracker/StTrackSegment.h
+++ StRoot/TPCCATracker/StTrackSegment.h
@@ -1,8 +1,8 @@
 #ifndef __StTrackSegment__
 #define __StTrackSegment__
+#include "Riostream.h"
 #include "AliHLTTPCCADef.h"
 #ifdef CALC_DCA_ON
-#include "Riostream.h"
 #include "AliHLTTPCCATrackParam.h"
 class trackInSector {// Inner parameters for track segment reconstruceted in a sector;
  public:
@@ -11,6 +11,6 @@ class trackInSector {// Inner parameters for track segment reconstruceted in a s
   float                 alpha;
   AliHLTTPCCATrackParam Track;
 };
-ostream&  operator<<(ostream& os, const trackInSector& v);
+std::ostream&  operator<<(std::ostream& os, const trackInSector& v);
 #endif /* CALC_DCA_ON */
 #endif /* __StTrackSegment__ */
