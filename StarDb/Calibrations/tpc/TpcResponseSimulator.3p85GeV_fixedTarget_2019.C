8adc9931e81f982317b976801b3dce90aeb41b23
Yuri Fisyak <fisyak@bnl.gov>
2024-01-01 12:20:52 -0500

    Clean up

--- StarDb/Calibrations/tpc/TpcResponseSimulator.3p85GeV_fixedTarget_2019.C
+++ StarDb/Calibrations/tpc/TpcResponseSimulator.3p85GeV_fixedTarget_2019.C
@@ -1,3 +1,6 @@
+#ifndef __CINT__
+#include "tables/TpcResponseSimulator_Table.h"
+#endif
 TDataSet *CreateTable() { 
   // -----------------------------------------------------------------
   // db/.const/StarDb/Calibrations/tpc/.TpcResponseSimulator/TpcResponseSimulator Allocated rows: 1  Used rows: 1  Row size: 124 bytes
