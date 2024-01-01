19ac0a9b4c8516b99d16edbf32cb0823ac435230
Yuri Fisyak <fisyak@bnl.gov>
2024-01-01 12:40:39 -0500

    Comdine all Cosmics in one file

---
+++ bin/GetYearFromPWD.pl
@@ -0,0 +1,20 @@
+#! /usr/bin/env perl
+use File::Basename;
+use Cwd;
+use Env;
+my $pwd = cwd(); #print "pwd = $pwd\n";
+my $year  = "";
+my $FIELD = "";
+if    ($pwd =~ /2019/) {$year = "2019";}
+elsif ($pwd =~ /2020/) {$year = "2020";}
+elsif ($pwd =~ /2021/) {$year = "2021";}
+elsif ($pwd =~ /2022/) {$year = "2022";}
+elsif ($pwd =~ /2023/) {$year = "2023";}
+if    ($pwd =~ /FFI/) {$FIELD = "FFI";}
+elsif ($pwd =~ /RFI/) {$FIELD = "RFI";}
+elsif ($pwd =~ /FF/) {$FIELD = "FF";}
+elsif ($pwd =~ /RF/) {$FIELD = "RF";}
+elsif ($pwd =~ /ZF/) {$FIELD = "ZF";}
+#$ENV{YEAR} = $year;
+#$ENV{FIELD} = $FIELD;
+print "$year\n";
