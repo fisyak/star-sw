4fc7b8d76a98ac4f88db299ea6f56e13f975d43b
Yuri Fisyak <fisyak@bnl.gov>
2024-01-05 12:08:06 -0500

    Add charge

--- macros/muMcdpTiR.C
+++ macros/muMcdpTiR.C
@@ -1,7 +1,7 @@
-void muMcdpTiR() {
+void muMcdpTiR(const Char_t *charge = "-") {
   TH3F *h3[2] = {0};
-  h3[0] = (TH3F *) gDirectory->Get("/Tracks/Global/Rec/All/(-)/EtapT/dPtiR");
-  h3[1] = (TH3F *) gDirectory->Get("/Tracks/Primary/Rec/All/(-)/EtapT/dPtiR");
+  h3[0] = (TH3F *) gDirectory->Get(Form("/Tracks/Global/Rec/All/(%s)/EtapT/dPtiR",charge));
+  h3[1] = (TH3F *) gDirectory->Get(Form("/Tracks/Primary/Rec/All/(%s)/EtapT/dPtiR",charge));
   if (! h3[0] || ! h3[1]) return;
   TObjArray *arr = new TObjArray(4);
   TString same;
