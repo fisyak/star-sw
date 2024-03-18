36ec5d1c1ed5d6cfa12125f89b86f83c8762a332
Yuri Fisyak <fisyak@bnl.gov>
2024-03-18 08:14:40 -0400

    Add Wheel for Alignment2024

---
+++ StarDb/Geometry/tpc/TpcWheelPosition.C
@@ -0,0 +1,15 @@
+#ifndef __CINT__
+#include "tables/St_Survey_Table.h"
+#endif
+
+TDataSet *CreateTable() { 
+  if (!gROOT->GetClass("St_Survey")) return 0;
+  Survey_st row = {0, 1,0,0, 0,1,0, 0,0,1, 0,0,0, 1e-5,1e-5,1e-4,4e-3,4e-3,4e-3,"Ideal"};
+  Int_t n = 2;// east = 0, west = 1
+  St_Survey *tableSet = new St_Survey("TpcWheelPosition",n);
+  for (Int_t half = 0; half <= 1; half++) {
+    row.Id = half;
+    tableSet->AddAt(&row.Id);
+  }
+  return (TDataSet *)tableSet;
+}
