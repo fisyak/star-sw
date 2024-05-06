d5cde29aa6f40b750e0e1d8037f5496914729c67
Yuri Fisyak <fisyak@bnl.gov>
2024-05-06 19:41:58 -0400

    fix typo

--- StRoot/StBFChain/BigFullChain.h
+++ StRoot/StBFChain/BigFullChain.h
@@ -1234,7 +1234,7 @@ Bfc_st BFC[] = { // standard chains
   {"CorrY"       ,"" ,"","ExB,OShortR,OBmap,OPr40,OIFC,OSectorAlign,NewTpcAlignment,OSpaceZ2,OGridLeakFull"
    ",-OBmap2D,-OClock,-OTwist"                                           ,"","","New Tpc Alignment",kFALSE},
   {"Alignment2024","" ,"",""                                        ,"","","Turn on Alignment 2024",kFALSE},
-  {"CorrZ","" ,"","Alignment2024.-CorrY,-CorrX,ExB,OShortR,OBmap,OPr40,OIFC,NewTpcAlignment,OSpaceZ2"
+  {"CorrZ","" ,"","Alignment2024,-CorrY,-CorrX,ExB,OShortR,OBmap,OPr40,OIFC,NewTpcAlignment,OSpaceZ2"
    ",OGridLeakFull,-OBmap2D,-OClock,-OTwist,-OSectorAlign"              ,"","","Tpc Alignment 2024",kFALSE},
   {"ExB"         ,""  ,"","",""                                       ,"","Activate ExB correction",kFALSE},
   {"EB1"         ,""  ,"","",""                                     ,"","Force ExB configuration 1",kFALSE},
