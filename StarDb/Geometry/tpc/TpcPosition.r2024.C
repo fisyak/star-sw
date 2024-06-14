d8f0cf06140e124e29f611f7c9a9d77441a2824d
Yuri Fisyak <fisyak@bnl.gov>
2024-06-14 12:08:24 -0400

    Reduce comment field to 31c

--- StarDb/Geometry/tpc/TpcPosition.r2024.C
+++ StarDb/Geometry/tpc/TpcPosition.r2024.C
@@ -6,7 +6,7 @@ TDataSet *CreateTable() {
   if (!TClass::GetClass("St_Survey")) return 0;
   Survey_st row = {
     //            -gamma     beta    gamma            -alpha    -beta    alpha                 x0        y0        z0   
-    {0, 1.00000,-0.00000,-0.00056, 0.00000, 1.00000,-0.00013, 0.00056, 0.00013, 1.00000,  -0.1423,  -0.2979,  -0.1618,0.,0.,0.,0.,0.,0.,"2024 TPC 03/04/2024 average of West and East"}
+    {0, 1.00000,-0.00000,-0.00056, 0.00000, 1.00000,-0.00013, 0.00056, 0.00013, 1.00000,  -0.1423,  -0.2979,  -0.1618,0.,0.,0.,0.,0.,0.,"2024 TPC 03/04/2024 av. W&E"}
   };
   St_Survey *tableSet = new St_Survey("TpcPosition",1);
   tableSet->AddAt(&row.Id);
