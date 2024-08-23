630a339cc370d9c4f33eea85a1084bb0e4d994fe
Yuri Fisyak <fisyak@bnl.gov>
2024-08-23 13:44:28 -0400

    Change default no. hit cut from 15 to 10

--- StRoot/StMuDSTMaker/COMMON/StMuDst.cxx
+++ StRoot/StMuDSTMaker/COMMON/StMuDst.cxx
@@ -109,8 +109,8 @@ TClonesArray** StMuDst::eztArrays                = 0;
 Int_t StMuDst::mCurrVertexId                     = -2;
 TObjArray* StMuDst::mCurrPrimaryTracks           = 0;
 #else /* __TFG__VERSION__ */
-Int_t StMuDst::MinNoTpcMcHits = 15;
-Int_t StMuDst::MinNoTpcRcHits = 15;
+Int_t StMuDst::MinNoTpcMcHits = 10;
+Int_t StMuDst::MinNoTpcRcHits = 10xs;
 Double_t StMuDst::fgerMax = 0;    // 50 um
 Double_t StMuDst::fgdca3Dmax = 0; // 50 cm
 Double_t StMuDst::fgVxXmin = 0, StMuDst::fgVxXmax = 0, StMuDst::fgVxYmin = 0, StMuDst::fgVxYmax = 0;
