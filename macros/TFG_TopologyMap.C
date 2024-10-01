ff4b2b3f97d9ce5e48885e4a53c7382f3afe0d4d
Yuri Fisyak <fisyak@bnl.gov>
2024-10-01 08:40:09 -0400

    Fix bug with track topology map for iTPC

--- macros/TFG_TopologyMap.C
+++ macros/TFG_TopologyMap.C
@@ -1,20 +1,16 @@
 #include "Rtypes.h"
+#include "Riostream.h"
 UInt_t fgMap[3] = {0};
 //________________________________________________________________________________
-Bool_t bit(int i) const
+Bool_t bit(int i)
 {
   if (i <= 31) return (fgMap[0]>>i & 1U);
   if (i <= 63) return (fgMap[1]>>(i-32) & 1U);
   return (fgMap[2]>>(i-64) & 1U);
 }
 //________________________________________________________________________________
-void  TFG_TopologyMap(UInt_t *map=0) {
-  if (! map) return;
-  fgMap = *map;
-}
-//________________________________________________________________________________
 void TFG_TopologyMap(UInt_t map0 = 0, UInt_t map1 = 0, UInt_t map2 = 0) {
-  //  if (! fgMap) fgMap = new UInt{3];
+  //  if (! fgMap) fgMap = new UInt[3];
   fgMap[0] = map0;
   fgMap[1] = map1;
   fgMap[2] = map2;
@@ -23,4 +19,11 @@ void TFG_TopologyMap(UInt_t map0 = 0, UInt_t map1 = 0, UInt_t map2 = 0) {
 Bool_t hasHitInRow(Int_t row) {
   if (row <= 45) return bit(row+7);
   return  bit(row+64-46);
+}
+//________________________________________________________________________________
+void PrintMap(UInt_t map0 = 0, UInt_t map1 = 0, UInt_t map2 = 0) {
+  TFG_TopologyMap(map0,map1,map2);
+  for (Int_t row = 1; row <= 72; row++) {
+    if (hasHitInRow(row)) cout << "has hit in " << row << " row" << endl;
+  }
 }
