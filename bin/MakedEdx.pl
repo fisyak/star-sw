2c14b1ab01438b919beaf3e9ac86193052e43e34
Yuri Fisyak <fisyak@bnl.gov>
2024-11-11 18:35:27 -0500

    Freeze

--- bin/MakedEdx.pl
+++ bin/MakedEdx.pl
@@ -2671,6 +2671,7 @@ my $debug = 0;
 #$hist = "RunXXIV02"; $NEvents = 5000; $disk = "/hlt/cephfs/reco/2024/RF/TFG24b"; $RECO = "/";  $Production = "pp200_LowLuminosity/"; $year = "*/*/"; $FILE = "hlt_"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 05/12/2024 
 #$hist = "RunXXIV03"; $NEvents = 5000; $disk = "/hlt/cephfs/reco/2024/RF/TFG24b"; $RECO = "/";  $Production = "pp200_*/"; $year = "*/*/"; $FILE = "hlt_"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 05/13/2024  TpcSecRowB
 #$hist = "RunXXIV04"; $NEvents = 5000; $disk = "/hlt/cephfs/"; $RECO = "/";  $Production = "reco/2024/RF/TFG24b/pp200_*/"; $year = "*/*/"; $FILE = "hlt_"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 05/13/2024  TpcSecRowB
+$hist = "RunXXIV05"; $NEvents = 5000; $disk = "/hlt/cephfs/"; $RECO = "/";  $Production = "reco/2024/RF/TFG24b/pp200_*/"; $year = "*/*/"; $FILE = "hlt_"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 11/05/2024  
 #################################### TFG24c ############################################
 #$hist = "RunTFG24c01"; $NEvents = 5000; $disk = "/gpfs01/star/subsysg/TFG/reco/TFG24c"; $RECO = "/";  $Production = "20*/RF/*GeV*/"; $year = "/*/"; $FILE = "st_"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 07/21/2024 check old calibration with TFG24c production
 #################################### DEV2 fixedTarget 2019-2021 ############################################
@@ -2696,7 +2697,7 @@ my $debug = 0;
 #$hist = "RunXIX_XXI20"; $NEvents = 500000; $disk = "/hlt/cephfs/"; $RECO = "";  $Production = "reco/dEdxCalib"; $year = "/2*/RF/*fixed*/*/"; $FILE = "*/"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 10/21/2024  TpcPadCorrectionMDC
 #$hist = "RunXIX_XXI21"; $NEvents = 500000; $disk = "/hlt/cephfs/"; $RECO = "";  $Production = "reco/dEdxCalib"; $year = "/2*/RF/*fixed*/*/"; $FILE = "*/"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 10/22/2024  TpcSecRowB
 #$hist = "RunXIX_XXI22"; $NEvents = 500000; $disk = "/hlt/cephfs/"; $RECO = "";  $Production = "reco/dEdxCalib"; $year = "/2*/RF/*fixed*/*/"; $FILE = "*/"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 10/23/2024  TpcLengthCorrectionMDN
-$hist = "RunXIX_XXI23"; $NEvents = 500000; $disk = "/hlt/cephfs/"; $RECO = "";  $Production = "reco/dEdxCalib"; $year = "/2*/RF/*fixed*/*/"; $FILE = "*/"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 10/24/2024  MySQL
+#$hist = "RunXIX_XXI23"; $NEvents = 500000; $disk = "/hlt/cephfs/"; $RECO = "";  $Production = "reco/dEdxCalib"; $year = "/2*/RF/*fixed*/*/"; $FILE = "*/"; $STAR_LEVEL = ".DEV2"; $select = "*";  $keep = 0; $Mode = 2; $macro = "dEdx";# 10/24/2024  MySQL
 if ($Year eq "/") {$Year = "2020";}
 my @badruns = ();
 my $prod = $hist; #$Production;
