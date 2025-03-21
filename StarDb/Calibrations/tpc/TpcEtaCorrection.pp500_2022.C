ed10984778aa7da2ded5caf7f9ea63925677ecfd
Yuri Fisyak <fisyak@bnl.gov>
2025-03-21 09:50:46 -0400

    RunXXII20

--- StarDb/Calibrations/tpc/TpcEtaCorrection.pp500_2022.C
+++ StarDb/Calibrations/tpc/TpcEtaCorrection.pp500_2022.C
@@ -7,28 +7,29 @@ TDataSet *CreateTable() {
   Int_t nrows = 2;
   St_tpcCorrection *tableSet = new St_tpcCorrection("TpcEtaCorrection",nrows);
   tpcCorrection_st row;
-  memset(&row,0,tableSet->GetRowSize()); // //net/l404/data/fisyak/Histograms/RunXXII12/Eta" 
-  row.idx   = 1;                         //  root.exe *.root Chain.C ,  tChaain->Draw("mu-muJ:y*y>>o","i&&j&&mu>-0.4&&abs(x)>40.5","prof")
+  memset(&row,0,tableSet->GetRowSize()); // ~/work/Histograms/RunXXII16 $ root.exe Eta3GPpp500_2022.root Eta3PGPpp500_2022.root Chain.C
+  row.idx   = 1;                         //   tChain->Draw("mu-muJ:y*y>>o","(i&&j&&mu>-0.4&&abs(x)>40.5)/dmu**2","profg")
   row.nrows = nrows;
-  row.npar =            6;//  o->Fit("pol5","e")
-  row.a[0] =  7.68487e-02;
-  row.a[1] = -1.46571e-01;
-  row.a[2] = -2.08002e-01;
-  row.a[3] =  3.15222e-01;
-  row.a[4] = -1.29356e-01;
-  row.a[5] =  1.71639e-02;
+  row.npar =            8;//  o->Fit("pol7","e")
+  row.a[0] =  9.39789e-02;
+  row.a[1] = -3.13606e-01;
+  row.a[2] =  1.66940e-01;
+  row.a[3] = -2.40561e-01;
+  row.a[4] =  4.62371e-01;
+  row.a[5] = -3.89208e-01;
+  row.a[6] =  1.48440e-01;
+  row.a[7] = -2.13348e-02;
   tableSet->AddAt(&row);
   memset(&row,0,tableSet->GetRowSize());
-  row.idx   = 2;                         //  tChain->Draw("mu-muJ:y*y>>i","i&&j&&mu>-0.4&&abs(x)<40.5","prof")
+  row.idx   = 2;                         //  tChain->Draw("mu-muJ:y*y>>i","(i&&j&&mu>-0.4&&abs(x)<40.5)/dmu**2","profg")
   row.nrows = nrows;
-  row.npar =            7;// i->Fit("pol6","e")
-  row.a[0] =  1.17781e-01; 
-  row.a[1] = -3.23740e-01;
-  row.a[2] =  5.37433e-01;
-  row.a[3] = -6.35513e-01;
-  row.a[4] =  3.60018e-01;
-  row.a[5] = -9.18971e-02;
-  row.a[6] =  8.64982e-03;
+  row.npar =            6;// i->Fit("pol5","e")
+  row.a[0] =  1.23525e-01; 
+  row.a[1] = -3.06953e-01;
+  row.a[2] =  2.16029e-01;
+  row.a[3] = -1.31120e-01;
+  row.a[4] =  4.96652e-02;
+  row.a[5] = -6.91850e-03;
   tableSet->AddAt(&row);
   return (TDataSet *)tableSet;
 }
--- StarDb/Calibrations/tpc/TpcZCorrectionC.pp500_2022.C
+++ StarDb/Calibrations/tpc/TpcZCorrectionC.pp500_2022.C
@@ -9,12 +9,12 @@ TDataSet *CreateTable() {
   row.min =  10.0;
   row.max = 220.0;
   row.npar =            6;// pp500_2022
-  row.a[0] =   -0.0064401;
-  row.a[1] =    0.0041693;
-  row.a[2] =  -9.0693e-05;
-  row.a[3] =   7.9915e-07;
-  row.a[4] =  -3.3999e-09;
-  row.a[5] =    5.635e-12;
+  row.a[0] =     0.012708;
+  row.a[1] =    0.0037244;
+  row.a[2] =   -8.101e-05;
+  row.a[3] =   6.8499e-07;
+  row.a[4] =  -2.8422e-09;
+  row.a[5] =   4.6701e-12;
   tableSet->AddAt(&row);
   memset(&row,0,tableSet->GetRowSize());
   row.idx   = 2;
@@ -22,12 +22,12 @@ TDataSet *CreateTable() {
   row.min =  10.0;
   row.max = 220.0;
   row.npar =            6;// pp500_2022
-  row.a[0] =   -0.0093259;
-  row.a[1] =    0.0037267;
-  row.a[2] =  -5.0702e-05;
-  row.a[3] =   2.6338e-07;
-  row.a[4] =  -8.3108e-10;
-  row.a[5] =   1.4389e-12;
+  row.a[0] =   -0.0009878;
+  row.a[1] =    0.0034222;
+  row.a[2] =  -4.3152e-05;
+  row.a[3] =   2.0117e-07;
+  row.a[4] =  -6.6083e-10;
+  row.a[5] =   1.3387e-12;
   tableSet->AddAt(&row);
   return (TDataSet *)tableSet;
 }
