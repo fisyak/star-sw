84b5483c887b8a21eb3fae7ce4abd4b0892374ff
Yuri Fisyak <fisyak@bnl.gov>
2025-04-11 15:04:57 -0400

    Freeze

--- macros/MuEbyE.C
+++ macros/MuEbyE.C
@@ -1,6 +1,7 @@
-///  root.exe 'lMuDst.C(-1,"","StEvent,Stu,MuDst,mysql,tpcDb,detDb,TFGdbOpt,CorrZ,magF,nodefault")' 'MuEbyE.C+("7p7GeV_EastOff/*/*/*.MuDst.root","7p7GeV_WestOff/*/*/*.MuDst.root","EbyE7p7GeV.root")'
+///  root.exe 'lMuDst.C(-1,"","StEvent,Stu,MuDst,picoEvt,mysql,tpcDb,detDb,TFGdbOpt,CorrZ,magF,nodefault")' 'MuEbyE.C+("7p7GeV_EastOff/*/*/*.MuDst.root","7p7GeV_WestOff/*/*/*.MuDst.root","EbyE7p7GeV.root")'
+///  root.exe 'lMuDst.C(-1,"","StEvent,Stu,MuDst,picoEvt,mysql,tpcDb,detDb,TFGdbOpt,CorrZ,magF,nodefault")' 'MuEbyE.C+("14p5GeV_2019_EastOff/118/20118006/st_physics_20118006_raw_1500002.MuDst.root","14p5GeV_2019_WestOff/118/20118006/st_physics_20118006_raw_3000001.MuDst.root","EbyE14p5GeV.root")'
 
-#if ! defined(__CINT__) && ! defined(__CLING__)
+#if ! defined(__CINT__) && ! defined(__CLING__) || defined(__MAKECINT__)
 #include "StBFChain/StBFChain.h"
 #include "StMuDSTMaker/COMMON/StMuDst.h"
 #include "StMuDSTMaker/COMMON/StMuEvent.h"
@@ -13,39 +14,119 @@
 #include "StDetectorDbMaker/St_starTriggerDelayC.h"
 #else
 class StMuDstMaker;
+class StMuDst;
 #endif
 StMuDstMaker *muDstMko = 0;
 StMuDstMaker *muDstMkn = 0;
 //void *muTbyT(StMuDst *oldMuDst, StMuDst *newMuDst);
+StMuDst *oldMuDst = 0;
+StMuDst *newMuDst = 0;
+StMuEvent *oldEv = 0, *newEv = 0;
+Int_t NoGTrkOld = 0, NoGTrkNew = 0;
+Int_t NoPvtxOld = 0, NoPvtxNew = 0;
+TFile *fOut   = 0;
+static Int_t _debug = 0;
 struct BPoint_t {
   Float_t dT, freq, DV, run, trigOff, time;//
 };
 const Char_t *vName = "dT:freq:DV:run:trigOff: time";
 BPoint_t BPoint;
-void MuEbyE(
-#if 0
-	       const Char_t *oldf="/gpfs02/eic/ayk/STAR/reco/MuDst/AuAu_200_production_2016/ReversedFullField/P16ij/2016/125/17125034/st_physics_adc_17125034_raw_1000007.MuDst.root", 
-	       const Char_t *newf="/gpfs01/star/pwg/fisyak/Embedding/2016/piNmTsq5PerCentZ6cm/st_physics_adc_17125034_raw_1000007.MuDst.root",
-#else
-	       const Char_t *oldf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/EastOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
-	       const Char_t *newf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/WestOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
-#endif
-	       const Char_t *out = "MuTbyT.root") {
-  TString fileO(out);
-  if (fileO == "") {
-    fileO = gSystem->BaseName(oldf);
-    fileO.ReplaceAll("MuDst.","");
+//________________________________________________________________________________
+void Fill(const Char_t *out = 0) {
+  static TNtuple *FitP = 0;
+  static TH2F *dX      = 0;
+  static TH2F *dY      = 0;
+  static TH2F *dZ      = 0;
+  static TH2F *dT      = 0;
+  static TH2F *dXY2[2];
+  static TH1F *dZ2[2];
+  static TH2F *XZ[2];
+  static TH2F *YZ[2];
+  if (out) {
+    TString fileO(out);
+    fOut   = new TFile(fileO,"recreate");
+    FitP = new TNtuple("FitP","dT versus run",vName);
+    dX      = new TH2F("dX","dX (West - East) versus Z; Z ; dX ",420,-210,210,400,-2.,2.); 
+    dY      = new TH2F("dY","dY (West - East) versus Z; Z ; dY ",420,-210,210,400,-2.,2.); 
+    dZ      = new TH2F("dZ","dZ (West - East) versus Z; Z ; dZ ",420,-210,210,400,-5.,5.); 
+    dT      = new TH2F("dT","dT(#musec) (West - East) versus Z ; Z ; dT",420,-210,210,1000,-0.5,0.5); 
+    XZ[0]   = new TH2F("XZW","X versus Z (West) ; Z ; X",420,-210,210,400,-2,2);
+    XZ[1]   = new TH2F("XZE","X versus Z (East) ; Z ; X",420,-210,210,400,-2,2);
+    YZ[0]   = new TH2F("YZW","Y versus Z (West) ; Z ; Y",420,-210,210,400,-2,2);
+    YZ[1]   = new TH2F("YZE","Y versus Z (East) ; Z ; Y",420,-210,210,400,-2,2);
+    dXY2[0] = new TH2F("dXY2W","dY versus dX of the second vertex (West) ; dX2 ; dY2",400,-2,2,400,-2,2);
+    dXY2[1] = new TH2F("dXY2E","dY versus dX of the second vertex (East) ; dX2 ; dY2",400,-2,2,400,-2,2);
+    dZ2[0]  = new TH1F("dZ2W","dZ of the second vertex (West) ; dZ2",400,-2,2);
+    dZ2[1]  = new TH1F("dZ2E","dZ of the second vertex (East) ; dZ2",400,-2,2);
+    return;
   }
-  TFile *fOut = new TFile(fileO,"recreate");
-  TNtuple *FitP = new TNtuple("FitP","dT versus run",vName);
-  TH2D *dX = new TH2D("dX","dX (West - East) versus Z; Z ; dX ",420,-210,210,400,-2.,2.); 
-  TH2D *dY = new TH2D("dY","dY (West - East) versus Z; Z ; dY ",420,-210,210,400,-2.,2.); 
-  TH2D *dZ = new TH2D("dZ","dZ (West - East) versus Z; Z ; dZ ",420,-210,210,400,-2.,2.); 
-  TH2D *dT = new TH2D("dT","dT(#musec) (West - East) versus Z ; Z ; dT",420,-210,210,1000,-0.5,0.5); 
-  TH2D *dXY2[2] = {new TH2D("dXY2W","dY versus dX of the second vertex (West) ; dX2 ; dY2",400,-2,2,400,-2,2),
-		   new TH2D("dXY2E","dY versus dX of the second vertex (East) ; dX2 ; dY2",400,-2,2,400,-2,2)};
-  TH1D *dZ2[2] = {new TH1D("dZ2W","dZ of the second vertex (West) ; dZ2",400,-2,2),
-		  new TH1D("dZ2E","dZ of the second vertex (East) ; dZ2",400,-2,2)};
+  // Require only one primary vertex in Z range [195,205];
+  StMuDst *MuDst[2] = {oldMuDst, newMuDst};
+  Bool_t okVX = kTRUE;
+  StMuPrimaryVertex *BestVtx[2] = {0,0};
+  for (Int_t k = 0; k < 2; k++) {// old new == West (EastOff)  East (WestOff)
+    MuDst[k]->SetInstance();
+    UInt_t NPV = MuDst[k]->numberOfPrimaryVertices();
+    if (! NPV) continue;
+    if (! MuDst[k]->primaryVertex(0)) continue;
+    // Find vertex with highest multiplicity (ignore rank)
+    Int_t MaxnoTracks = 0;
+    for(UInt_t  iPV=0; iPV < NPV; iPV++) {
+      StMuPrimaryVertex *Vtx = MuDst[k]->primaryVertex(iPV);
+      if (! Vtx) continue;
+      if (_debug) {cout << k << ":\t"; Vtx->Print();}
+      Int_t noTracks = Vtx->noTracks();
+      if (noTracks > MaxnoTracks) {
+	BestVtx[k] = Vtx;
+	MaxnoTracks = noTracks;
+      }
+    }
+    if (! BestVtx[k]) continue;
+    Double_t Z_ref = BestVtx[k]->position().z();
+    for(UInt_t  iPV=0; iPV < NPV; iPV++) {
+      StMuPrimaryVertex *Vtx = MuDst[k]->primaryVertex(iPV);
+      if (! Vtx) continue;
+      if (Vtx == BestVtx[k]) continue;
+      if (_debug) {cout << k << ": Extra\t"; Vtx->Print();}
+      if (TMath::Abs(Vtx->position().z() - Z_ref) < 5.0) {
+	dXY2[k]->Fill(Vtx->position().x() - BestVtx[k]->position().x(), Vtx->position().y() - BestVtx[k]->position().y());
+	dZ2[k]->Fill(Vtx->position().z() - BestVtx[k]->position().z());
+	okVX = kFALSE;
+      }
+    }
+  }
+  if (okVX && BestVtx[0] &&  BestVtx[1]) {
+    StThreeVectorF xyzD = (BestVtx[0]->position() - BestVtx[1]->position())/2; 
+    if (_debug) {
+      cout << "West\t"; BestVtx[0]->Print();
+      cout << "East\t"; BestVtx[1]->Print();
+      cout << "Diff:\t" << xyzD << endl;
+    }
+    for (Int_t k = 0; k < 2; k++) {
+      XZ[k]->Fill(BestVtx[k]->position().z(), BestVtx[k]->position().x());
+      YZ[k]->Fill(BestVtx[k]->position().z(), BestVtx[k]->position().y());
+    }
+    if (TMath::Abs(xyzD.z()) > 5.0) return;
+    StThreeVectorF xyzA = (BestVtx[0]->position() + BestVtx[1]->position())/2;
+    Double_t driftVel = StTpcDb::instance()->DriftVelocity()*1e-6;
+    dX->Fill(xyzA.z(),xyzD.x());
+    dY->Fill(xyzA.z(),xyzD.y());
+    dZ->Fill(xyzA.z(),xyzD.z());
+    BPoint.dT = xyzD.z()/driftVel;
+    BPoint.freq = StTpcDb::instance()->Electronics()->samplingFrequency();
+    BPoint.DV = driftVel;
+    BPoint.trigOff = St_starTriggerDelayC::instance()->TrigT0();
+    BPoint.run = muDstMko->GetRunNumber();
+    BPoint.time = muDstMko->GetDateTime().Convert();
+    dT->Fill(xyzA.z(),BPoint.dT);
+    FitP->Fill(&BPoint.dT);
+  }
+}
+//________________________________________________________________________________
+void MuEbyE(const Char_t *oldf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/EastOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
+	    const Char_t *newf="/gpfs01/star/subsys-tpc/fisyak/reco/2018/WestOff/st_physics_adc_19116021_raw_1000030.MuDst.root",
+	    const Char_t *out = "MuTbyT.root") {
+  Fill(out);
   muDstMko = new StMuDstMaker(0,0,oldf,"","",99999,"MuDstOld"); // no East
   muDstMkn = new StMuDstMaker(0,0,newf,"","",99999,"MuDstNew"); // no West
   StMuDstMaker *muDstMk[2] = {muDstMko, muDstMkn};
@@ -59,18 +140,15 @@ void MuEbyE(
   TChain *chainN = muDstMkn->chain();
   //  chainO->AddFriend(chainN );
   chainN->BuildIndex("MuEvent.mRunInfo.mRunId","MuEvent.mEventInfo.mId");
-  StMuDst *oldMuDst = 0;
-  StMuDst *newMuDst = 0;
-  StMuEvent *oldEv = 0, *newEv = 0;
-  Int_t NoGTrkOld = 0, NoGTrkNew = 0;
-  Int_t NoPvtxOld = 0, NoPvtxNew = 0;
-  static Int_t _debug = 0;
   Int_t nev = 0;
   Long64_t nentries = chainO->GetEntries();
   if (nentries <= 0) return;
+#if 0
   Int_t ok = chain->MakeEvent();
-  Bool_t FXT = St_beamInfoC::instance()->IsFixedTarget();
+#endif
+  //  Bool_t FXT = St_beamInfoC::instance()->IsFixedTarget();
   UInt_t NPV[2];
+#if 0
   while (! ok) {
     if (_debug) cout << "================================================================================" << endl;
     oldMuDst = muDstMko->muDst(); oldMuDst->SetInstance(); oldEv = oldMuDst->event(); if (_debug) {cout << "Old:"; oldMuDst->Print();}
@@ -88,56 +166,7 @@ void MuEbyE(
       if (nev%1000 == 1) {
 	cout << "event " << nev << "\trun = " << oldEv->runId() << "\tevents Old = " << oldEv->eventId() << "\tnew = " << newEv->eventId() 
 	     << " has matched" << endl;
-      }
-      // Require only one primary vertex in Z range [195,205];
-      StMuDst *MuDst[2] = {oldMuDst, newMuDst};
-      Bool_t okVX = kTRUE;
-      StMuPrimaryVertex *BestVtx[2] = {0,0};
-      for (Int_t k = 0; k < 2; k++) {// old new == West (EastOff)  East (WestOff)
-	MuDst[k]->SetInstance();
-	UInt_t NPV = MuDst[k]->numberOfPrimaryVertices();
-	if (! NPV) continue;
-	if (! MuDst[k]->primaryVertex(0)) continue;
-	// Find vertex with highest multiplicity (ignore rank)
-	Int_t MaxnoTracks = 0;
-	for(UInt_t  iPV=0; iPV < NPV; iPV++) {
-	  StMuPrimaryVertex *Vtx = MuDst[k]->primaryVertex(iPV);
-	  if (! Vtx) continue;
-	  Int_t noTracks = Vtx->noTracks();
-	  if (noTracks > MaxnoTracks) {
-	    BestVtx[k] = Vtx;
-	    MaxnoTracks = noTracks;
-	  }
-	}
-	if (! BestVtx[k]) continue;
-	Double_t Z_ref = BestVtx[k]->position().z();
-	for(UInt_t  iPV=0; iPV < NPV; iPV++) {
-	  StMuPrimaryVertex *Vtx = MuDst[k]->primaryVertex(iPV);
-	  if (! Vtx) continue;
-	  if (Vtx == BestVtx[k]) continue;
-	  if (_debug) Vtx->Print();
-	  if (TMath::Abs(Vtx->position().z() - Z_ref) < 5.0) {
-	    dXY2[k]->Fill(Vtx->position().x() - BestVtx[k]->position().x(), Vtx->position().y() - BestVtx[k]->position().y());
-	    dZ2[k]->Fill(Vtx->position().z() - BestVtx[k]->position().z());
-	    okVX = kFALSE;
-	  }
-	}
-      }
-      if (okVX && BestVtx[0] &&  BestVtx[1]) {
-	StThreeVectorF xyzD = (BestVtx[0]->position() - BestVtx[1]->position())/2; 
-	StThreeVectorF xyzA = (BestVtx[0]->position() + BestVtx[1]->position())/2;
-	Double_t driftVel = StTpcDb::instance()->DriftVelocity()*1e-6;
-	dX->Fill(xyzA.z(),xyzD.x());
-	dY->Fill(xyzA.z(),xyzD.y());
-	dZ->Fill(xyzA.z(),xyzD.z());
-	BPoint.dT = xyzD.z()/driftVel;
-	BPoint.freq = StTpcDb::instance()->Electronics()->samplingFrequency();
-	BPoint.DV = driftVel;
-	BPoint.trigOff = St_starTriggerDelayC::instance()->TrigT0();
-	BPoint.run = muDstMko->GetRunNumber();
-	BPoint.time = muDstMko->GetDateTime().Convert();
-	dT->Fill(xyzA.z(),BPoint.dT);
-	FitP->Fill(&BPoint.dT);
+	Fill();
       }
     } else {
       cout << "event " << nev << "\trun = " << oldEv->runId() << "\tevents Old = " << oldEv->eventId() << "\tnew = " << newEv->eventId() 
@@ -146,5 +175,33 @@ void MuEbyE(
   NEXT_EVENT:
     ok = chain->MakeEvent();
   }
+#else
+  while (1) {
+    Int_t ok = chain->MakeEvent();
+    if (ok) break;
+    oldMuDst = muDstMko->muDst(); if (_debug) {cout << "Old:"; oldMuDst->Print("B");}
+    oldEv = oldMuDst->event();
+    newMuDst = muDstMkn->muDst(); if (_debug) {cout << "New:"; newMuDst->Print("B");}
+    newEv = newMuDst->event(); 
+    if (oldEv->runId() != newEv->runId() || oldEv->eventId() != newEv->eventId()) {
+      muDstMkn->Clear(); 
+      Int_t okN = muDstMkn->Make(oldEv->runId(), oldEv->eventId()); 
+      if (okN) continue;
+      if (oldEv->runId() != newEv->runId() || oldEv->eventId() != newEv->eventId()) {
+	cout << "event " << nev << "\trun = " << oldEv->runId() << "\tevents Old = " << oldEv->eventId() << "\tnew = " << newEv->eventId() 
+	     << " has not matched" << endl;
+	continue;
+      }
+      newMuDst = muDstMkn->muDst(); if (_debug) {cout << "New:"; newMuDst->Print("B");}
+      newEv = newMuDst->event();
+    }
+    nev++;
+    if (nev%1000 == 1) {
+      cout << "event " << nev << "\trun = " << oldEv->runId() << "\tevents Old = " << oldEv->eventId() << "\tnew = " << newEv->eventId() 
+	   << " has matched" << endl;
+    }
+    Fill();
+  }
+#endif
   fOut->Write();
 }
---
+++ macros/Oleg.C
@@ -0,0 +1,30 @@
+void Oleg(Double_t Z = 635, const Char_t *histName = "flux100keVn") {
+  TFile *_file0 = TFile::Open("2013/pp510/flux_2013xPythiaPP510.root"); //  Nov 16  2014
+  TFile *_file1 = TFile::Open("r2013VMC/r2013VMC.root");
+  TFile *_file2 = TFile::Open("r2013G3/r2013G3.root");
+  TFile *_file3 = TFile::Open("r2022VMC/r2022VMC.root");
+  TFile *_file4 = TFile::Open("r2022G3/r2022G3.root");
+  TFile *files[5] = {_file0, _file1,  _file2, _file3, _file4};
+  const Char_t *tags[5] = {"2013G3old", "2013VMC", "2013G3", "2022VMC", "2022G3"};
+  Int_t binX = -1;
+  TLegend *l = new TLegend(0.5,0.6,0.8,0.9);
+  TString same("");
+  THStack *hstack = 0; // new THStack("hs",histName);
+  for (Int_t i = 0; i < 5; i++) {
+    TH2 *flux = (TH2*) files[i]->Get(histName);
+    if (binX < 0) {
+      binX = flux->GetXaxis()->FindBin(Z);
+      hstack = new THStack(Form("S%s",flux->GetName()),Form("%s at Z = %6.1f",flux->GetTitle(),Z));
+    }
+    TString Title = gSystem->DirName(flux->GetDirectory()->GetName());
+    TH1D *proj = flux->ProjectionY(tags[i], binX, binX);
+    proj->SetLineColor(i+1);
+    proj->SetMarkerColor(i+1);
+    proj->SetXTitle("R (cm)");
+    //    proj->Draw(same); same = "sames";
+    hstack->Add(proj);
+    l->AddEntry(proj,tags[i]);
+  }
+  hstack->Draw("nostack");
+  l->Draw();
+}
