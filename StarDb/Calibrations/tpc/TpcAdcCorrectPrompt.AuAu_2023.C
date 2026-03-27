acd88d533f241875598c1c77f4e1fde1b970e6a8
Yuri Fisyak <fisyak@bnl.gov>
2026-03-27 11:12:58 -0400

    Freeze version with separated fit for all partcles

--- StarDb/Calibrations/tpc/TpcAdcCorrectPrompt.AuAu_2023.C
+++ StarDb/Calibrations/tpc/TpcAdcCorrectPrompt.AuAu_2023.C
@@ -12,30 +12,31 @@ TDataSet *CreateTable() {
   Int_t nrows = 2;
   St_tpcCorrection *tableSet = new St_tpcCorrection("TpcAdcCorrectPrompt",nrows);
   memset(&row,0,tableSet->GetRowSize());
-  //    [l409] /hlt/cephfs/reco/TPC23/2023 $ root.exe SparseGGAdcSparse7.root */SparseGGAdcSparse7.root Chain.C
-  /*
-    tChain->Draw("mu:z3>>OPrompt(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.04&&dmu<0.02&&sigma<0.02&&j==2&&z2>203","colz")
-    TF1 *off = new TF1("off4","TMath::Log(1.+[0]*TMath::Exp(-x))+pol2(1)",3,10.2)
-    OPrompt->Fit("pol1")
-   */
+  /* wihtout He3 and alpha 
+[cvmfswrite06] ~/TFG/TPC23/2025 $ root.exe COLE/electron+/SparseGPAdcSparse8.root COLE/electron-/SparseGPAdcSparse8.root COLE/kaon+/SparseGPAdcSparse8.root COLE/kaon-/SparseGPAdcSparse8.root COLE/pion+/SparseGPAdcSparse8.root COLE/pion-/SparseGPAdcSparse8.root COLE/proton+/SparseGPAdcSparse8.root COLE/proton-/SparseGPAdcSparse8.root COLE/triton/SparseGPAdcSparse8.root FXTE/deuteron/SparseGPAdcSparse8.root FXTE/electron+/SparseGPAdcSparse8.root FXTE/electron-/SparseGPAdcSparse8.root FXTE/kaon+/SparseGPAdcSparse8.root FXTE/kaon-/SparseGPAdcSparse8.root FXTE/pion+/SparseGPAdcSparse8.root FXTE/pion-/SparseGPAdcSparse8.root FXTE/proton+/SparseGPAdcSparse8.root FXTE/proton-/SparseGPAdcSparse8.root FXTE/triton/SparseGPAdcSparse8.root Chain.C
+    tChain->Draw("mu:z3>>O(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.02&&dmu>0&&dmu<0.01&&j==2&&z2>203","colz")
+    TF1 *off1 = new TF1("off1","TMath::Log(1.+[0]*TMath::Exp(-x))+pol1(1)",3,10.2)
+    O->Fit("pol1")
+  */
   row.type     =           10; //
   row.idx      =            1; //
   row.nrows    =        nrows; //
   row.npar     =            2; //
-  row.a[0]     =     0.722057; //   +/-   0.00919842 
-  row.a[1]     =   -0.0532346; //   +/-   0.00164662-
+  row.a[0]     =     0.761155;//   +/-   0.00723945  0.722057; //   +/-   0.00919842 
+  row.a[1]     =   -0.0595495;//   +/-   0.00136358-0.0532346; //   +/-   0.00164662-
   tableSet->AddAt(&row);     // 
   memset(&row,0,tableSet->GetRowSize());
   /* 
-    tChain->Draw("mu:z3>>IPrompt(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.04&&dmu<0.02&&sigma<0.02&&j==1&&z2>203","colz")
-    IPrompt->Fit("pol1")
+    tChain->Draw("mu:z3>>I(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.02&&dmu>0&&dmu<0.01&&j==1&&z2>203","colz")
+    I->Fit("pol2")
    */
   row.type     =           10; //
   row.idx      =            2; //
   row.nrows    =        nrows; //
-  row.npar     =            2; //
-  row.a[0]     =     0.738988; //   +/-   0.00901403 
-  row.a[1]     =   -0.0556767; //   +/-   0.00166946 
+  row.npar     =            3; //
+  row.a[0]     =    -0.385137; //   +/-   0.175303   0.738988; //   +/-   0.00901403 
+  row.a[1]     =     0.485637; //   +/-   0.0847559-0.0556767; //   +/-   0.00166946 
+  row.a[2]     =   -0.0635574; //   +/-   0.0101669
   tableSet->AddAt(&row);     // 
 // ----------------- end of code ---------------
  return (TDataSet *)tableSet;
--- StarDb/Calibrations/tpc/TpcAdcCorrectionC.AuAu_2023.C
+++ StarDb/Calibrations/tpc/TpcAdcCorrectionC.AuAu_2023.C
@@ -12,9 +12,9 @@ TDataSet *CreateTable() {
   Int_t nrows = 2;
   St_tpcCorrection *tableSet = new St_tpcCorrection("TpcAdcCorrectionC",nrows);
   memset(&row,0,tableSet->GetRowSize());
-  //    [l409]  /hlt/cephfs/reco/TPC23/2025 $ root.exe 'lDb.C(0,"AuAu_2025,Corrz")' */*/SparseGPAdcSparse8.root Chain.C
-  /*
-    tChain->Draw("mu:z3>>O(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.04&&dmu<0.02&&sigma<0.02&&j==2&&z2<203","colz")
+  /* wihtout He3 and alpha 
+[cvmfswrite06] ~/TFG/TPC23/2025 $ root.exe COLE/electron+/SparseGPAdcSparse8.root COLE/electron-/SparseGPAdcSparse8.root COLE/kaon+/SparseGPAdcSparse8.root COLE/kaon-/SparseGPAdcSparse8.root COLE/pion+/SparseGPAdcSparse8.root COLE/pion-/SparseGPAdcSparse8.root COLE/proton+/SparseGPAdcSparse8.root COLE/proton-/SparseGPAdcSparse8.root COLE/triton/SparseGPAdcSparse8.root FXTE/deuteron/SparseGPAdcSparse8.root FXTE/electron+/SparseGPAdcSparse8.root FXTE/electron-/SparseGPAdcSparse8.root FXTE/kaon+/SparseGPAdcSparse8.root FXTE/kaon-/SparseGPAdcSparse8.root FXTE/pion+/SparseGPAdcSparse8.root FXTE/pion-/SparseGPAdcSparse8.root FXTE/proton+/SparseGPAdcSparse8.root FXTE/proton-/SparseGPAdcSparse8.root FXTE/triton/SparseGPAdcSparse8.root Chain.C
+    tChain->Draw("mu:z3>>O(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.02&&dmu>0&&dmu<0.01&&j==2&&z2<203","colz")
     TF1 *off1 = new TF1("off1","TMath::Log(1.+[0]*TMath::Exp(-x))+pol1(1)",3,10.2)
     O->Fit(off1)
   */
@@ -22,22 +22,22 @@ TDataSet *CreateTable() {
   row.idx      =            1; //
   row.nrows    =        nrows; //
   row.npar     =            2; //
-  row.OffSet   =       7.3107; //   +/-   1.2361    
-  row.a[0]     =     0.595928; //   +/-   0.0149052 
-  row.a[1]     =   -0.0333872; //   +/-   0.00198954
+  row.OffSet   =      5.28057;//   +/-   0.5106         7.3107; //   +/-   1.2361    
+  row.a[0]     =     0.673227;//   +/-   0.0107744    0.595928; //   +/-   0.0149052 
+  row.a[1]     =   -0.0437482;//   +/-   0.00155876 -0.0333872; //   +/-   0.00198954
   tableSet->AddAt(&row);      // 
   memset(&row,0,tableSet->GetRowSize());
   /* 
-    tChain->Draw("mu:z3>>I(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.04&&dmu<0.02&&sigma<0.02&&j==1&&z2<203","colz")
+    tChain->Draw("mu:z3>>I(70,3,10,200,0.,1.)","dsigma>0&&dsigma<0.02&&dmu>0&&dmu<0.01&&j==1&&z2<203","colz")
      I->Fit(off1)
   */
   row.type     =           10; //
   row.idx      =            2; //
   row.nrows    =        nrows; //
   row.npar     =            2; //
-  row.OffSet   =      7.87712; //   +/-   1.15075    
-  row.a[0]     =     0.608362; //   +/-   0.0156548  
-  row.a[1]     =   -0.0321323; //   +/-   0.00210247 
+  row.OffSet   =      10.8046;//   +/-   0.625754    7.87712; //   +/-   1.15075    
+  row.a[0]     =     0.736502;//   +/-   0.0162701  0.608362; //   +/-   0.0156548  
+  row.a[1]     =   -0.0510437;//   +/-   0.002447 -0.0321323; //   +/-   0.00210247 
   tableSet->AddAt(&row);     // 
 // ----------------- end of code ---------------
  return (TDataSet *)tableSet;
